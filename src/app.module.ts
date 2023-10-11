fbbde478764fcbb43f94ca1042bcd2239ddc6858
itslucassouza <lucasvini193@hotmail.com>
2023-10-10 22:16:46 -0300

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./modules/users/users.module";
+import { DatabaseModule } from './shared/database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ConflictException, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { PrismaService } from "src/shared/database/prisma.service";
 import { hash } from "bcryptjs";
+import { UsersRepository } from "src/shared/database/repositories/users.repositories";
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly usersRepo: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
 
-    const emailTaken = await this.prismaService.user.findFirst({
+    const emailTaken = await this.usersRepo.findUnique({
       where: {
         email,
       },
@@ -23,7 +23,7 @@ export class UsersService {
 
     const hashedPassword = await hash(password, 12);
 
-    const user = await this.prismaService.user.create({
+    const user = await this.usersRepo.create({
       data: {
         name,
         email,
