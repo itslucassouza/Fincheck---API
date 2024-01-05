6cd5f8dad7e9450660c99d69df36c5ef39844f68
itslucassouza <lucasvini193@hotmail.com>
2024-01-04 23:22:57 -0300

    feature: Add new create bankccount method

--- src/modules/bank-accounts/dto/create-bank-account.dto.ts
+++ src/modules/bank-accounts/dto/create-bank-account.dto.ts
@@ -1,1 +1,20 @@
-export class CreateBankAccountDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { BankAccountType } from "../entities/BankAccount";
+
+export class CreateBankAccountDto {
+    @IsString()
+    @IsNotEmpty()
+    name: String;
+
+    @IsNumber()
+    @IsNotEmpty()
+    initialBalance: Number;
+
+    @IsString()
+    @IsEnum(BankAccountType)
+    type: BankAccountType;
+
+    @IsString()
+    @IsNotEmpty()
+    color: String;
+}
